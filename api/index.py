a57d8b4bd156cb07bd194fd8bcb29bbe7b907330
Dhairya Shah <dtshah24022006@gmail.com>
2024-08-06 20:55:49 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, request
+from duckduckgo_search import DDGS
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def hello_world():
+    search_term = request.args.get('q')
+    results = DDGS().text(search_term, max_results=5)
+    return results
 
-@app.route('/about')
-def about():
-    return 'About'
+# if __name__ == '__main__':
+#     app.run(debug=True, port=3000)
